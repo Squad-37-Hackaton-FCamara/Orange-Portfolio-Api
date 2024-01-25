e99b678a85c96bc856400c7330053945369b5202
Larissa Lavínia <larissa11.cedaspy@gmail.com>
2024-01-24 23:35:19 -0300

    [Feat] - Criando servidor express

---
+++ src/shared/infra/http/serve.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import  { projetoRotas }  from './routes/projetoRotas'
+import  { usuarioRotas }  from './routes/usuarioRotas'
+
+const porta = process.env.PORT || 3000
+const url = `http://localhost:${porta}`
+const app = express()
+
+
+app.use(express.json(), cors())
+
+app.use(projetoRotas, usuarioRotas)
+
+app.listen(porta, function () {
+    console.log(`Servidor rodando na porta ${porta}!🚀`)
+})
