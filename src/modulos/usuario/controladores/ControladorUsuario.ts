60400a38be5a03499256828a56b268ebfcc2b660
evanildo <evanildo.sf91@gmail.com>
2024-01-25 10:53:49 -0300

    feat: criar o controlador de usuarios

---
+++ src/modulos/usuario/controladores/ControladorUsuario.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express'
+import { RepositorioUsuario } from '../repositorios/repositorioUsuario'
+import { ServicoCriarUsuario } from '../servicos/ServicoCriarUsuario'
+
+class ControladorUsuario {
+    public async criar(req: Request, res: Response): Promise<Response> {
+        const { nome, sobrenome, email, senha } = req.body
+
+        const servicoCriarUsuario = new ServicoCriarUsuario(
+            new RepositorioUsuario()
+        )
+
+        const usuario = await servicoCriarUsuario.executar({
+            nome,
+            sobrenome,
+            email,
+            senha
+        })
+
+        return res.status(201).json(usuario)
+    }
+}
+
+export { ControladorUsuario }
