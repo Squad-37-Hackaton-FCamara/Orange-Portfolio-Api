b6d410ba7e58732d414ff185161ea124e9518135
evanildo <evanildo.sf91@gmail.com>
2024-01-24 22:18:15 -0300

    feat: criar migrações para criar tabelas no banco de dados

---
+++ prisma/migrations/20240125011026_create_table_user_and_projects/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "usuarios" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "sobrenome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "foto" TEXT,
+    "nacionalidade" TEXT,
+
+    CONSTRAINT "usuarios_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "projetos" (
+    "id" TEXT NOT NULL,
+    "titulo" TEXT NOT NULL,
+    "tags" TEXT[],
+    "link" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "foto" TEXT NOT NULL,
+    "usuario_id" TEXT NOT NULL,
+
+    CONSTRAINT "projetos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "usuarios_email_key" ON "usuarios"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "projetos_link_key" ON "projetos"("link");
+
+-- AddForeignKey
+ALTER TABLE "projetos" ADD CONSTRAINT "projetos_usuario_id_fkey" FOREIGN KEY ("usuario_id") REFERENCES "usuarios"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
