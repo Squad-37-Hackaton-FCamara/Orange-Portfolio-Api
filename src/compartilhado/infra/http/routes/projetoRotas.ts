00e576851a7df8a45d4cb4fffce8b853895c5744
Larissa Lavínia <larissa11.cedaspy@gmail.com>
2024-01-28 00:54:30 -0300

    [Feat] - Chamar controlador de editar na rota

--- src/compartilhado/infra/http/routes/projetoRotas.ts
+++ src/compartilhado/infra/http/routes/projetoRotas.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import { ControladorProjeto } from '../../../../modulos/projeto/controladores/ControladorProjeto'
+import { tratamentoImagemIntermediario } from '../middlewares/salvarImagemMidleware'
 
 const projetoRotas = express()
 
 projetoRotas.get('/projeto', new ControladorProjeto().listar)
 projetoRotas.get('/projeto/:userId')
-projetoRotas.post('/projeto', new ControladorProjeto().criar)
-projetoRotas.put('/projeto')
+projetoRotas.post('/projeto', tratamentoImagemIntermediario, new ControladorProjeto().criar)
+projetoRotas.put('/projeto/:id', tratamentoImagemIntermediario, new ControladorProjeto().editar)
 projetoRotas.delete('/projeto')
 
 
