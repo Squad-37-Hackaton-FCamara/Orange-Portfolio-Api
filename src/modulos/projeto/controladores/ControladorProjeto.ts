787f1070e367ab90d1ca7190e6c3d33acf175a30
Larissa Lavínia <larissa11.cedaspy@gmail.com>
2024-01-28 00:55:08 -0300

    [Feat] - Implementação do controlador de editar

--- src/modulos/projeto/controladores/ControladorProjeto.ts
+++ src/modulos/projeto/controladores/ControladorProjeto.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { RepositorioProjeto } from '../repositorios/repositorioProjeto'
 import { ServicoCriarProjeto } from '../servicos/ServicoCriarProjeto'
 import { ServicoListarProjeto } from '../servicos/ServicoListarProjeto'
+import { ServicoEditarProjeto } from '../servicos/ServicoEditarProjeto'
 
 class ControladorProjeto {
     public async criar(req: Request, res: Response): Promise<Response> {
@@ -27,7 +28,7 @@ class ControladorProjeto {
     }
 
     public async listar(req: Request, res: Response): Promise<Response> {
-        
+
         const servicoListarProjeto = new ServicoListarProjeto(
             new RepositorioProjeto()
         )
@@ -35,6 +36,29 @@ class ControladorProjeto {
         const projetos = await servicoListarProjeto.executar()
 
         return res.status(201).json(projetos)
+    }
+
+    public async editar(req: Request, res: Response): Promise<Response> {
+        const { id } = req.params
+        const { titulo, tags, link, descricao, foto, usuario_id } = req.body
+
+        const servicoEditarProjeto = new ServicoEditarProjeto(
+            new RepositorioProjeto()
+        )
+
+        const projeto = await servicoEditarProjeto.executar(id, {
+            titulo,
+            tags,
+            link,
+            descricao,
+            foto,
+            usuario_id,
+            req,
+            res
+
+        })
+
+        return res.status(201).json(projeto)
     }
 }
 
