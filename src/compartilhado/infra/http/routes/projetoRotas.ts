52ef2f7369149c49c88db7d11f41df3ca589cc12
Larissa Lavínia <larissa11.cedaspy@gmail.com>
2024-01-28 01:50:14 -0300

    [Feat] - Adicionar controlador de excluir projeto na rota

--- src/compartilhado/infra/http/routes/projetoRotas.ts
+++ src/compartilhado/infra/http/routes/projetoRotas.ts
@@ -8,7 +8,7 @@ projetoRotas.get('/projeto', new ControladorProjeto().listar)
 projetoRotas.get('/projeto/:usuario_id', new ControladorProjeto().listarPeloUserId)
 projetoRotas.post('/projeto', tratamentoImagemIntermediario, new ControladorProjeto().criar)
 projetoRotas.put('/projeto/:id', tratamentoImagemIntermediario, new ControladorProjeto().editar)
-projetoRotas.delete('/projeto')
+projetoRotas.delete('/projeto/:id', new ControladorProjeto().excluir)
 
 
 export { projetoRotas }
