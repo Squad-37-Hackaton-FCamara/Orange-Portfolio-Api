3765908f12af69616682877311006a145d881857
Larissa Lavínia <larissa11.cedaspy@gmail.com>
2024-01-28 01:15:50 -0300

    [Feat] - Interface de listar projeto pelo id do usuário

--- src/modulos/projeto/interfaces/IRepositorioProjeto.ts
+++ src/modulos/projeto/interfaces/IRepositorioProjeto.ts
@@ -5,4 +5,5 @@ export interface IRepositorioProjeto {
     criar(data: ICriarProjeto): Promise<IProjeto>
     editar(id: String, data: ICriarProjeto): Promise<IProjeto>
     listar(): Promise<IProjeto[] | null>
+    listarPeloUserId(id: String): Promise<IProjeto[] | null>
 }
