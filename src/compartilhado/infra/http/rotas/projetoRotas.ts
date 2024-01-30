36c4e19d4328611405ed8a4853a2441dcd790f52
GcellyGomes <gicellygomes.gomes@gmail.com>
2024-01-30 01:08:13 -0300

    feat: intermediario pra autenticacao

--- src/compartilhado/infra/http/rotas/projetoRotas.ts
+++ src/compartilhado/infra/http/rotas/projetoRotas.ts
@@ -2,23 +2,26 @@ import express from 'express'
 import { ControladorProjeto } from '../../../../modulos/projeto/controladores/ControladorProjeto'
 import { tratamentoImagemIntermediario } from '../intermediario/tratarImagem'
 import validarRequisicao from '../intermediario/validarCampos'
+import autenticado from '../intermediario/autenticacao'
 import { projetoSchema } from '../schemas/projetoSchema'
 
 const projetoRotas = express()
 
-projetoRotas.get('/projeto', new ControladorProjeto().listar)
-projetoRotas.get('/projeto/:usuario_id', new ControladorProjeto().listarPeloUserId)
+projetoRotas.get('/projeto', autenticado, new ControladorProjeto().listar)
+projetoRotas.get('/projeto/:usuario_id', autenticado, new ControladorProjeto().listarPeloUserId)
 projetoRotas.post('/projeto',
+    autenticado,
     tratamentoImagemIntermediario,
     validarRequisicao(projetoSchema),
     new ControladorProjeto().criar
 )
 projetoRotas.put('/projeto/:id',
+    autenticado,
     tratamentoImagemIntermediario,
     validarRequisicao(projetoSchema),
     new ControladorProjeto().editar
 )
-projetoRotas.delete('/projeto/:id', new ControladorProjeto().excluir)
+projetoRotas.delete('/projeto/:id', autenticado, new ControladorProjeto().excluir)
 
 
 export { projetoRotas }
