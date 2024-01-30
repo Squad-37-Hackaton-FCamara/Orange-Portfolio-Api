0747f16964c37754f6e6a23dd33b19de8f4d04d3
Larissa Lavínia <larissa11.cedaspy@gmail.com>
2024-01-29 22:53:19 -0300

    [Fix] - Refatoração da classe para tratamento de erro

--- src/modulos/usuario/repositorios/RepositorioAutenticacao.ts
+++ src/modulos/usuario/repositorios/RepositorioAutenticacao.ts
@@ -1,37 +1,36 @@
 import autenticacaoConfig from '@config/autenticacao'
 import { compare } from 'bcrypt'
 import { Secret, sign } from 'jsonwebtoken'
-import { AppError } from 'src/compartilhado/errors/AppError'
 import prismaCliente from 'src/compartilhado/infra/prisma'
 import { IAutenticacao } from '../interfaces/IAutenticacao'
 import { IRepositorioAutenticacao } from '../interfaces/IRepositorioAutenticacao'
 import { IUsuarioAutenticado } from '../interfaces/IUsuarioAutenticado'
+import { ErroPersonalizado } from 'src/compartilhado/erros/Erros'
 
 export class RepositorioAutenticacao implements IRepositorioAutenticacao {
     public async autenticar({
         email,
         senha
     }: IAutenticacao): Promise<IUsuarioAutenticado> {
-        // Verificar se o email existe no banco de dados
+
         const usuario = await prismaCliente.usuario.findFirst({
             where: { email },
             include: { projetos: true }
         })
 
         if (!usuario) {
-            throw new AppError('Usuário/senha incorreto', 401)
+            throw new ErroPersonalizado('E-mail ou senha incorretos!', 400)
         }
 
-        // Verificar se a senha está correta
         const senhaVerificada = await compare(senha, usuario.senha)
 
         if (!senhaVerificada) {
-            throw new AppError('Usuário/senha incorreto', 401)
+            throw new ErroPersonalizado('E-mail ou senha incorretos!', 400)
         }
 
-        // Gerar token
         const token = sign(
             { id: usuario.id },
+
             autenticacaoConfig.jwt.secret as Secret,
             {
                 subject: usuario.id,
