843ab6c8a3b7c0829b4b6108e09340e842fed01f
Larissa Lavínia <larissa11.cedaspy@gmail.com>
2024-02-02 18:42:08 -0300

    [fix] - Corrigindo inconsistencia no banco

--- src/compartilhado/infra/prisma/migrations/20240202214117_orange_portifolio/migration.sql
+++ src/compartilhado/infra/prisma/migrations/20240202214117_orange_portifolio/migration.sql
@@ -18,7 +18,7 @@ CREATE TABLE "projetos" (
     "link" TEXT NOT NULL,
     "descricao" TEXT NOT NULL,
     "foto" TEXT NOT NULL,
-    "Type '{ id: string; titulo: string; tags: string[]; link: string; descricao: string; foto: string; createAt: Date; usuario_id: string; }' is not assignable to type 'IProjeto'.ts(2322)" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "usuario_id" TEXT NOT NULL,
 
     CONSTRAINT "projetos_pkey" PRIMARY KEY ("id")
